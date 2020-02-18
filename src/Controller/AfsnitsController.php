ba8160b30ae80feae8fa9c8c49e700f185d6391b
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2020-02-18 22:30:14 +0100

    Projektet revurderet og under omstrukturering

--- src/Controller/AfsnitsController.php
+++ src/Controller/AfsnitsController.php
@@ -32,24 +32,33 @@ class AfsnitsController extends AbstractController
      */
     public function index()
     {
-    	$teksten = array ('St 3-1' => '011244-2048  Tove Jensen', 
-    										'St 3-2' => '123465-5194  Hans Larsen', 
-    										'St 1-1' => '', 
+    	$pliste=   array ('St 3-1' => '011244-2048  Tove Jensen',
+    										'St 3-2' => '123465-5194  Hans Larsen',
+    										'St 1-1' => '',
     										'St 1-2' => '131548-1561  Birger Jensen',
     										'St 2-1' => '',
-    										'St 2-2' => '213456-1545  Helle Madsen', 
-    										'St 5'   => '', 
-    										'St 6'   => '', 
-    										'St 7'   => '181415-1478  Dolly Parton', 
-    										'St 8'   => '', 
-    										
+    										'St 2-2' => '213456-1545  Helle Madsen',
+    										'St 5'   => '',
+    										'St 6'   => '',
+    										'St 7'   => '181415-1478  Dolly Parton',
+    										'St 8'   => '',
+
     										);
-    										
-      $this->session->set('indlagte', $teksten);
-    	
-    	
+
+    	    	$teksten = array ('Anamnese'=> 'Test1',
+    										'CNS' => 'Test2',
+    										'Pulm'=> 'Test3',
+    										'Card'=> 'Test4',
+    										'Gas' => 'Test5',
+    										'Ren' => 'Test6', );
+
+
+
+
         return $this->render('afsnits/index.html.twig', [
             'controller_name' => 'AfsnitsController',
+            'indlagte' => $pliste,
+            'mydata' => $teksten,
         ]);
     }
 }
