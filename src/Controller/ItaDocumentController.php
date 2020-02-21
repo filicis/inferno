34a745e52f29c0f6a0b6a105126255d2550fd151
Michael Lindhardt Rasmussen <mlr@dadlnet.dk>
2020-02-21 22:32:25 +0100

    Sidespring omkring SVG

--- src/Controller/ItaDocumentController.php
+++ src/Controller/ItaDocumentController.php
@@ -14,13 +14,38 @@ class ItaDocumentController extends AbstractController
 
     public function index(SessionInterface $session)
     {
-          // Er brugeren logget p� ?
+      $tekst= array(
+        "Klokken",
+        "Morfin (mg/iv)",
+        "Toradol mg/iv",
+        "Oxynorm mg/iv",
+        "Pethidin mg/iv",
+        "DHB mg/ml",
+        "Ondansetron mg/iv",
+        "Caps Oxynorm mg",
+        "T Paracetamol",
+        "",
+        "",
+        "VAS Score", 
+        "EPI ml/t",
+        "PCA",
+        "Timediurese",  
+        "Oxygen",
+        "Saturation",
+        "Kvalme",
+        "Forbinding",
+        "Blødning"    
+         
+        );  
+        
+          // Er brugeren logget på ?
         $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
         $afsnit= $session->get('afsnit');
 
         return $this->render('ita_document/index.html.twig', [
             'controller_name' => 'ItaDocumentController',
+            'tekst' => $tekst,
         ]);
     }
 }
